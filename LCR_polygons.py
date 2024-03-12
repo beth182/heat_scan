2692619fd084ecde3d4e4f1901faef38fb341bb2
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-03-12 16:51:51 +0000

    overlay polygon and cmip data

--- LCR_polygons.py
+++ LCR_polygons.py
@@ -1,9 +1,8 @@
 import os
 import geopandas as gpd
 import pandas as pd
+from shapely.geometry import Point
 import matplotlib.pyplot as plt
-from shapely.geometry import Point, Polygon, MultiPolygon
-import fiona
 
 current_dir = os.getcwd().replace('\\', '/') + '/'
 
@@ -50,9 +49,50 @@ for index, row in polygon_df.iterrows():
         min_distance = distance
         closest_polygon = row['geometry']
 
-if min_distance == 0:
+if min_distance != 0:
     print('end')
 
 # check w/ plot
 # gpd.GeoSeries([closest_polygon]).plot()
 # plt.scatter(point.x, point.y, c='r')
+
+
+
+# check w/ data
+
+import xarray as xr
+import gcsfs
+
+# Analysis of Google Cloud CMIP6 data using Pangeo tools.
+
+# instruction: https://medium.com/pangeo/cmip6-in-the-cloud-five-ways-96b177abe396
+# variable name table: https://pcmdi.llnl.gov/mips/cmip3/variableList.html#Table_A1f
+# Link to pangeo-cmip6.csv: https://cmip6.storage.googleapis.com/pangeo-cmip6.csv
+
+current_dir = os.getcwd().replace('\\', '/') + '/'
+
+zstore = "gs://cmip6/CMIP6/HighResMIP/CMCC/CMCC-CM2-HR4/highresSST-present/r1i1p1f1/Amon/tas/gn/v20170706/"
+
+fs = gcsfs.GCSFileSystem(token='anon', access='read_only')
+mapper = fs.get_mapper(zstore)
+
+# open it using xarray and zarr
+ds = xr.open_zarr(mapper, consolidated=True)
+
+# change lon from degrees east to between -180 to 180
+ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180
+ds = ds.sortby(ds.lon)
+
+# To plot the first time step
+# ds.tas.isel(time=0).plot()
+
+
+print('end')
+
+# fig
+fig = plt.figure(figsize=(8, 7))
+ax = plt.subplot(1, 1, 1)
+ds.tas.isel(time=0).plot(ax=ax)
+polygon_df.plot(ax=ax)
+gpd.GeoSeries([closest_polygon]).plot(ax=ax, facecolor='r')
+ax.scatter(point.x, point.y, c='k')
