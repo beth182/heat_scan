acea2e6c01b5d30203bb2ea9f7911d4fc2f1a4d8
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-03-19 19:51:28 +0000

    Update UTCI plot for Matthias

--- UTCI_plot_POI.py
+++ UTCI_plot_POI.py
@@ -7,7 +7,6 @@ import matplotlib as mpl
 import glob
 import os
 
-
 mpl.use('TkAgg')
 
 mpl.rcParams.update({'font.size': 15})
@@ -39,14 +38,23 @@ labelDict = {'Balbala_open_average_djf': 'Open 2007-01-21',
              'Balbala_tree_hot_jja': 'Tree 2015-08-13'}
 
 # Average current, average future
-labelDict_ = {'Balbala_open_average_djf': 'Open present climate',
-              'Balbala_open_average_jja': 'Open present climate',
-              'Balbala_open_hot_djf': 'Open future climate',
-              'Balbala_open_hot_jja': 'Open future climate',
-              'Balbala_tree_average_djf': 'Tree present climate',
-              'Balbala_tree_average_jja': 'Tree present climate',
-              'Balbala_tree_hot_djf': 'Tree future climate',
-              'Balbala_tree_hot_jja': 'Tree future climate'}
+# labelDict_ = {'Balbala_open_average_djf': 'Open present climate',
+#               'Balbala_open_average_jja': 'Open present climate',
+#               'Balbala_open_hot_djf': 'Open future climate',
+#               'Balbala_open_hot_jja': 'Open future climate',
+#               'Balbala_tree_average_djf': 'Tree present climate',
+#               'Balbala_tree_average_jja': 'Tree present climate',
+#               'Balbala_tree_hot_djf': 'Tree future climate',
+#               'Balbala_tree_hot_jja': 'Tree future climate'}
+
+labelDict_ = {'Balbala_open_average_djf': 'Open: Historical',
+              'Balbala_open_average_jja': 'Open: Historical',
+              'Balbala_open_hot_djf': 'Open: Future',
+              'Balbala_open_hot_jja': 'Open: Future',
+              'Balbala_tree_average_djf': 'Tree: Historical',
+              'Balbala_tree_average_jja': 'Tree: Historical',
+              'Balbala_tree_hot_djf': 'Tree: Future',
+              'Balbala_tree_hot_jja': 'Tree: Future'}
 
 # ax = plt.subplot(1, 1, 1, projection='polar')
 fig = plt.figure(figsize=(12, 6), constrained_layout=True)
@@ -100,55 +108,29 @@ for poifile in poifiles:
                 temp_color = 'magenta'
     # Summer days
     if poi.DOY.to_numpy()[0] > 90:
-        ax1.plot(temp_hour, temp_utci, label=labelDict_[temp_label], color=temp_color, linestyle=temp_line, marker = temp_mark)
+        ax1.plot(temp_hour, temp_utci, label=labelDict_[temp_label], color=temp_color, linestyle=temp_line,
+                 marker=temp_mark)
     # Winter days
     else:
-        ax2.plot(temp_hour, temp_utci, label=labelDict_[temp_label], color=temp_color, linestyle=temp_line, marker = temp_mark)
+        ax2.plot(temp_hour, temp_utci, label=labelDict_[temp_label], color=temp_color, linestyle=temp_line,
+                 marker=temp_mark)
 
-# ax1.legend(prop={'size': 8})
 ax1.set_ylabel('UTCI ($^\circ$C)')
-# ax1.set_xlabel('Hour')
 ax1.set_ylim(10, 50)
 ax1.set_xlim(6, 20)
-ax1.set_title('Summer', c='red')
-# ax1.grid(True)
-
-ax1.spines['bottom'].set_color('red')
-ax1.spines['top'].set_color('red')
-ax1.xaxis.label.set_color('red')
-ax1.tick_params(axis='x', colors='red')
-
-ax1.spines['left'].set_color('red')
-ax1.spines['right'].set_color('red')
-ax1.yaxis.label.set_color('red')
-ax1.tick_params(axis='y', colors='red')
+ax1.set_title('Summer')
+ax1.grid(True)
 
 ax1.legend()
 
-# ax2.legend(prop={'size': 8}, loc='upper left')
-# ax2.set_ylabel('UTCI [$^\circ$C]')
-# ax2.set_xlabel('Hour')
 ax2.set_ylim(10, 50)
 ax2.set_yticklabels([])
 ax2.set_xlim(6, 20)
-ax2.set_title('Winter', c='blue')
-# ax2.grid(True)
-
-ax2.spines['bottom'].set_color('blue')
-ax2.spines['top'].set_color('blue')
-ax2.xaxis.label.set_color('blue')
-ax2.tick_params(axis='x', colors='blue')
-
-ax2.spines['left'].set_color('blue')
-ax2.spines['right'].set_color('blue')
-ax2.tick_params(axis='y', colors='blue')
+ax2.set_title('Winter')
+ax2.grid(True)
 
 # Shared xlabel
-fig.supxlabel('Hour', x=0.515)
-
-# # Add shared legend at position in loc
-# handles, labels = ax1.get_legend_handles_labels()
-# fig.legend(handles, labels, loc=(0.434, 0.78), facecolor='white', framealpha=1)
+fig.supxlabel('Hour', x=0.515, y=0.05)
 
 fig.tight_layout()
 fig.savefig(save_path + 'UTCI.png', dpi=300)
