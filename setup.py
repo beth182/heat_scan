cb0338768e00c1d7959b1167f5969428fdc4a2a0
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-04-04 12:44:21 +0100

    install package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='heat_scan',
+    url='https://github.com/beth182/heat_scan',
+    author='Beth Saunders',
+    author_email='bethany.saunders@pgr.reading.ac.uk',
+    packages=[],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'geopandas',
+        'pandas',
+        'shapely'
+    ],
+    version='0.1',
+    license='MIT',
+    description='todo The description text',
+    long_description='todo The long description text'
+)
