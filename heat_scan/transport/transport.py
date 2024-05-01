de66b2890d94ef836cbbb5902a4b196376dbde52
beth182 <bethany.saunders@pgr.reading.ac.uk>
2024-05-01 15:50:50 +0100

    Main fun wip

--- heat_scan/transport/transport.py
+++ heat_scan/transport/transport.py
@@ -4,22 +4,41 @@ from heat_scan.tools.pangeo_CMIP import pangeo_CMIP_funs
 from heat_scan.tools import constants
 from heat_scan.transport import plotting_funs
 
+
+def main_find_CMIP(year=None):
+    """
+
+    :param save_path:
+    :return:
+    """
+    # ToDo: function to search the csv for user's choices here
+
+    # where the zstore is returned
+
+    # tasmax
+    # r1i1p1f1
+    # NOAA-GFDL
+    # ScenarioMIP
+    # ssp245
+    # day
+    zstore = 'gs://cmip6/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/ssp245/r1i1p1f1/day/tasmax/gr1/v20180701/'
+    # zstore = 'gs://cmip6/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/ssp585/r1i1p1f1/day/tasmax/gr1/v20180701/'
+
+    # zstore = 'gs://cmip6/CMIP6/ScenarioMIP/CCCma/CanESM5/ssp245/r1i1p2f1/day/tasmax/gn/v20190429/'
+
+    # zstore = 'gs://cmip6/CMIP6/ScenarioMIP/BCC/BCC-CSM2-MR/ssp245/r1i1p1f1/day/tasmax/gn/v20190318/'
+
+    # find the dataset
+    ds = pangeo_CMIP_funs.cmip6_via_pangeo(zstore=zstore)
+
+    # if there is a target year, subset the dataset by that year
+    if year != None:
+        ds = ds.sel(time=ds.time.dt.year.isin([year]))
+
+    return ds
+
+
 save_path = os.getcwd().replace('\\', '/') + '/'
-
-# tasmax
-# r1i1p1f1
-# NOAA-GFDL
-# ScenarioMIP
-# ssp245
-# day
-zstore = 'gs://cmip6/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/ssp245/r1i1p1f1/day/tasmax/gr1/v20180701/'
-# zstore = 'gs://cmip6/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/ssp585/r1i1p1f1/day/tasmax/gr1/v20180701/'
-
-# zstore = 'gs://cmip6/CMIP6/ScenarioMIP/CCCma/CanESM5/ssp245/r1i1p2f1/day/tasmax/gn/v20190429/'
-
-# zstore = 'gs://cmip6/CMIP6/ScenarioMIP/BCC/BCC-CSM2-MR/ssp245/r1i1p1f1/day/tasmax/gn/v20190318/'
-
-ds = pangeo_CMIP_funs.cmip6_via_pangeo(zstore=zstore)
 
 # plot data
 # straight variable at a given time
@@ -33,7 +52,6 @@ ds = pangeo_CMIP_funs.cmip6_via_pangeo(zstore=zstore)
 year = 2050
 # year = 2015
 
-ds_2015 = ds.sel(time=ds.time.dt.year.isin([year]))
 
 # define threshold
 threshold_temp = 30 + constants.convert_kelvin
